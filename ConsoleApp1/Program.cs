24fdbf58a72348628a09209a3758d21b06f42f67
Nahkala Petri <ao166551@gradia.fi>
2018-10-01 12:01:41 +0300

    Added consoleapp

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Tehtävä_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int zxc = 0;
+            int tulos = 0;
+            while (tulos < 10)
+            {
+
+                int qwe = peruna(10);
+                zxc += qwe;
+                tulos++;
+            }
+            Console.Write("Lukujen summa on: " + zxc);
+            Console.ReadKey();
+        }
+        static int peruna(int asd)
+        {
+            Console.Write("Anna kokonaisluku: ");
+            String k = Console.ReadLine();
+            int kokonaisluku = int.Parse(k);
+            return kokonaisluku;
+        }
+    }
+}
